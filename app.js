98fef2f48e0ac4ac6dbe7753b5c0b653bc3248b2
Angie <arco527@gmail.com>
2019-10-23 14:45:49 -0500

    Params

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    if(req.query.nombre){
-        res.send(`<h1>Hola ${req.query.nombre}!</h1>`)
-    }else{
-        res.send(`<h1>Hola desconocido!</h1>`)
-  }
+// app.get('/makers/:nombre', (req, res) => {
+//     if(req.params.nombre){
+//         res.send(`<h1>Hola ${req.params.nombre}!</h1>`)
+//     }else{
+//         res.send(`<h1>Hola desconocido!</h1>`)
+//   }
+// })
+
+
+
+app.get('/makers/:name', (req, res) => {
+var name = req.params.name
+  res.send('Hola ' + name.charAt(0).toUpperCase() + name.slice(1));
 })
+
 
 app.listen(3000, () => console.log('Listening on port 3000'));
